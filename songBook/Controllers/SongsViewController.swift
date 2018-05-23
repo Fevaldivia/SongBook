fd3b79a349ae053582f3244fe3804fba4394f7e2
Felipe <fe.valdiviaa@gmail.com>
2018-05-22 21:26:10 -0500

    add songs API request
    add song API request parsing JSON and showing the title of the songs in
    the table view.

--- songBook/Controllers/SongsViewController.swift
+++ songBook/Controllers/SongsViewController.swift
@@ -11,20 +11,67 @@ import UIKit
 
 class SongsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let rockSongs = [
-        "Back in Black",
-        "Highway to Hell",
-        "Wabash Cannonball",
-        "Dream On",
-        "Toys in the Attic",
-        "Planet Rock",
-        "Ramblin' Man",
-        "Whipping Post"
-    ]
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.getSongs()
+        self.tableView.reloadData()
+    }
+    
+    var songs = [AnyObject]()
+    
+    func getSongs() {
+        let urlString = "http://api.guitarparty.com/v2/songs/?query=All"
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue( Constants.API.APIKey, forHTTPHeaderField: "Guitarparty-Api-Key")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) {data, response, error in
+            if error != nil {
+                print("It Was an error in the task")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data to return")
+                return
+            }
+            
+            let parsedResult: [String:AnyObject]!
+            
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+            }catch{
+                print("There was an error parsing the data!")
+                return
+            }
+            guard let songsDictonary = parsedResult["objects"] as? [[String:AnyObject]] else {
+                print("We Couldn't get any song")
+                return
+            }
+            
+            for everySong in songsDictonary {
+                self.songs.append(everySong as AnyObject)
+            }
+            
+            self.performUIUpdatesOnMain {
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return self.rockSongs.count
+        return self.songs.count
     }
     
     
@@ -32,11 +79,14 @@ class SongsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let cellID = "songsCell"
         
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID)!
-        let favoriteThingForRow = self.rockSongs[(indexPath as NSIndexPath).row]
-        cell.textLabel?.text = favoriteThingForRow
+        let song = self.songs[(indexPath as NSIndexPath).row]
         
+        if let songTitle = song["title"] as? String {
+            self.performUIUpdatesOnMain {
+                cell.textLabel?.text = songTitle
+            }
+        }
         return cell
-        
     }
 
 }
---
+++ songBook/Model/GuitarPartyAPI/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  songBook
+//
+//  Created by Felipe Valdivia on 5/20/18.
+//  Copyright © 2018 Felipe Valdivia. All rights reserved.
+//
+
+struct Constants {
+    struct API {
+        static let ApiUrl = "http://api.guitarparty.com/v2"
+        static let allSongs = "http://api.guitarparty.com/v2/songs/?query=all"
+        static let APIKey = "791aa55b42d4e8ecd07f09634f72303c174aa954"
+    }
+}
+
