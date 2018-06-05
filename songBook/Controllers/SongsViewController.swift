91c82c5051ef60428999b95411c2998fe11f07b5
Felipe <fe.valdiviaa@gmail.com>
2018-06-04 20:30:10 -0500

    add closure for search by artist
    add new file to store closure function for the search method by artist
    add completition handler for the function and refactor tableview methods
    to use the new closure syntax.

--- songBook/Controllers/SongsViewController.swift
+++ songBook/Controllers/SongsViewController.swift
@@ -78,66 +78,31 @@ class SongsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     
-    //MARK: Search method
+    //MARK: Search method closure
     
     @IBAction func searchSongButton(_ sender: Any) {
         if (searchText.text?.isEmpty)! {
             print("There is no song to search.")
         }else{
-            let escapedURL = searchText.text!.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
-            let urlString = "http://api.guitarparty.com/v2/songs/?query=\(escapedURL!)"
-            let url = URL(string: urlString)
-            var request = URLRequest(url: url!)
-            
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue( Constants.API.APIKey, forHTTPHeaderField: "Guitarparty-Api-Key")
-            
-            let session = URLSession.shared
-            
-            let task = session.dataTask(with: request) {data, response, error in
-                if error != nil {
-                    print("It Was an error in the task")
+            GuitarFunctions.sharedInstanse.searchText(searchByArtist: searchText.text!) { (result, error) in
+                guard (result != nil || error == nil) else {
+                    print("Result is nil")
                     return
                 }
                 
-                guard let data = data else {
-                    print("No data to return")
-                    return
-                }
-                
-                let parsedResult: [String:AnyObject]!
-                
-                do {
-                    parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
-                }catch{
-                    print("There was an error parsing the data!")
-                    return
-                }
-                guard let songsDictonary = parsedResult["objects"] as? [[String:AnyObject]] else {
-                    print("We Couldn't get any song")
-                    return
-                }
-                
-                
-                // remove my last search from the array
-                self.songs.removeAll()
-                
-                for filter in songsDictonary {
-                    self.songs.append(filter as AnyObject)
-                }
+                GuitarFunctions.sharedInstanse.songs = result!
                 
                 self.performUIUpdatesOnMain {
                     self.tableView.reloadData()
                 }
             }
-            task.resume()
         }
     }
     
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return self.songs.count
+           return GuitarFunctions.sharedInstanse.songs.count
         
     }
     
@@ -146,7 +111,7 @@ class SongsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             let cellID = "songsCell"
             
             let cell = tableView.dequeueReusableCell(withIdentifier: cellID)!
-            let song = self.songs[(indexPath as NSIndexPath).row]
+            let song = GuitarFunctions.sharedInstanse.songs[indexPath.row]
             
             if let songTitle = song["title"] as? String {
                 self.performUIUpdatesOnMain {
@@ -160,7 +125,7 @@ class SongsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailSong = self.storyboard!.instantiateViewController(withIdentifier: "detailSong") as! SongDetailViewController
         
-        detailSong.song = self.songs[(indexPath as NSIndexPath).row] as! [String:AnyObject]
+        detailSong.song = GuitarFunctions.sharedInstanse.songs[(indexPath as NSIndexPath).row] as! [String:AnyObject]
         
         self.navigationController!.pushViewController(detailSong, animated: true)
     }
