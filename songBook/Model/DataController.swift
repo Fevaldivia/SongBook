b30756698125f1e6d1ea54b6fb087fff11420930
Felipe <fe.valdiviaa@gmail.com>
2018-06-08 20:31:10 -0500

    add coredata context
    add coredata context and store to manipulate the data in the app load
    the data controller in app delegate.

--- songBook/Model/DataController.swift
+++ songBook/Model/DataController.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import CoreData
+
+class DataController {
+    let persistantContainer:NSPersistentContainer
+    
+    var viewContext:NSManagedObjectContext {
+        return persistantContainer.viewContext
+    }
+    
+    init(modelName:String) {
+        persistantContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil){
+        persistantContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+                return
+            }
+            completion?()
+        }
+    }
+}
